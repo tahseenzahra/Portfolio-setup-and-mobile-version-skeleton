f1bb82d28c8d813d692f03e7a9d9500a7b3fa0ec
tahseenzahra <tahseen1zahra@gmail.com>
2022-05-18 13:07:25 +0500

    Add menu toggle at mobile display

---
+++ script/mobileMenu.js
@@ -0,0 +1,23 @@
+function showMenu() {
+  document.querySelector('#mobile-menu').style.display = 'inline-block';
+  document.querySelector('.mobile-sectionLinks').style.display = 'flex';
+  document.querySelector('header').style.display = 'none';
+  document.querySelector('.headline').style.display = 'none';
+  document.querySelector('.worksection').style.display = 'none';
+  document.querySelector('.FlashCards').style.display = 'none';
+  document.querySelector('.MainAboutme').style.display = 'none';
+  document.querySelector('.footer').style.display = 'none';
+}
+
+function hideMenu() {
+  document.querySelector('#mobile-menu').style.display = 'none';
+  document.querySelector('header').style.display = 'block';
+  document.querySelector('.headline').style.display = 'block';
+  document.querySelector('.worksection').style.display = 'block';
+  document.querySelector('.FlashCards').style.display = 'block';
+  document.querySelector('.MainAboutme').style.display = 'block';
+  document.querySelector('.footer').style.display = 'block';
+}
+
+document.getElementById('dropdown-id').addEventListener('click', showMenu);
+document.getElementById('closebtn-id').addEventListener('click', hideMenu);
